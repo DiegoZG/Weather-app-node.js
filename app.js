1bb69555be272b4890baeea63e091a3a8969fe9e
Diwo777 <diego.zegarra79@icloud.com>
2020-09-18 23:26:08 -0400

    pulled inf from weather api

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log('hello weather')
+const request = require('request')
+
+const url = 'http://api.weatherstack.com/current?access_key=1153e1fb0c0cb8f91b86df4bfc806a98&query=37.8267,-122.4233'
+
+request({ url: url }, (error, response) => {
+    const data = JSON.parse(response.body)
+    console.log(data.current)
+})
