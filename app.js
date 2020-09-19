9774c3dd7b23d42e65883d18e17b7072d3605ff6
Diwo777 <diego.zegarra79@icloud.com>
2020-09-19 17:44:24 -0400

    added mapbox api

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const request = require('request')
 
-const url = 'http://api.weatherstack.com/current?access_key=1153e1fb0c0cb8f91b86df4bfc806a98&query=37.8267,-122.4233'
+const url = 'http://api.weatherstack.com/current?access_key=1153e1fb0c0cb8f91b86df4bfc806a98&query=37.8267,-122.4233&units=f'
 
-request({ url: url }, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data.current)
+request({ url: url, json: true }, (error, response) => {
+    console.log(`${response.body.current.weather_descriptions[0]}. It is currently ${response.body.current.temperature} degrees out. It feel like ${response.body.current.feelslike} degrees out`)
+})
+
+
+const mapurl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiZGllZ296ZWdhcnJhNzgiLCJhIjoiY2tmYTV3eGQ2MDN5aTM0dDFqZHBkbW9taiJ9.lSRYHMRQ8NrP0VceVr1_mA&limit=1'
+
+request({url: mapurl, json: true}, (error, response) => {
+    console.log(`${response.body.features[0].center[1]}, ${response.body.features[0].center[0]} `)
 })
